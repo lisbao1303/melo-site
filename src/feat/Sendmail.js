35d67de0adfb881ea6bab1f49c80c663283c0c6a
lisbao1303 <gabriel.lisbao@gmail.com>
2020-04-29 15:58:06 -0300

    email netlify right

--- src/feat/Sendmail.js
+++ src/feat/Sendmail.js
@@ -1,7 +1,29 @@
 import React from "react";
 
 function Sendmail() {
-  return <h1></h1>;
+  return (
+    <form name="contact" method="post">
+      <input type="hidden" name="form-name" value="contact" />
+      <p>
+        <label>
+          Your Name: <input type="text" name="name" />
+        </label>
+      </p>
+      <p>
+        <label>
+          Your Email: <input type="email" name="email" />
+        </label>
+      </p>
+      <p>
+        <label>
+          Message: <textarea name="message"></textarea>
+        </label>
+      </p>
+      <p>
+        <button type="submit">Send</button>
+      </p>
+    </form>
+  );
 }
 
 export default Sendmail;
