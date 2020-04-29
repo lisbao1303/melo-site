0baaeaf6b154bbb1fa6d0a1aed8622d0d6b86e26
lisbao1303 <gabriel.lisbao@gmail.com>
2020-04-29 14:07:39 -0300

    test 2 estado

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
 import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+const nodemailer = require("nodemailer");
+
+let transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: true,
+  auth: {
+    user: "gabriel.lisbao@gmail.com",
+    pass: "youcan'tseeme",
+  },
+});
 
 function App() {
   const [teste, settest] = useState("esperando comando");
@@ -8,6 +19,27 @@ function App() {
     <div className="conteinerprincipal">
       <h1>O estado irá mudar: {teste}</h1>
       <button type="button" onClick={() => settest("mudança de estado")}>
+        clique aqui
+      </button>
+      <button
+        type="button"
+        onClick={() =>
+          transporter
+            .sendMail({
+              from: "Testa <gabriel.lisbao@gmail.com>",
+              to: "gabriel.lisbao@gmail.com",
+              subject: "Assubnto?",
+              text: "texto do meu email aqui ululu",
+              html: "<h1>CARAI mano</h1>",
+            })
+            .then((message) => {
+              console.log(message);
+            })
+            .catch((err) => {
+              console.log(err);
+            })
+        }
+      >
         clique aqui
       </button>
     </div>
