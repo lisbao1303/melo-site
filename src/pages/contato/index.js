32771ac6c2d207a4f7e594bcd07a9e5d1ed7423d
lisbao1303 <gabriel.lisbao@gmail.com>
2020-05-03 23:07:14 -0300

    sera

--- src/pages/contato/index.js
+++ src/pages/contato/index.js
@@ -31,7 +31,11 @@ const Contato = ({ match }) => {
                     exact
                     path={match.url}
                     render={() => (
-                      <Form name="contact" action="/" method="post">
+                      <Form
+                        name="contact"
+                        action={`${match.url}/enviado`}
+                        method="post"
+                      >
                         <input type="hidden" name="form-name" value="contact" />
                         <FormGroup>
                           <Label>Nome:</Label>
