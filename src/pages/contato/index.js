a2fb26d61213c0f8acb52a1970556a756bbbb00f
lisbao1303 <gabriel.lisbao@gmail.com>
2020-05-04 00:37:58 -0300

    se

--- src/pages/contato/index.js
+++ src/pages/contato/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Route } from "react-router-dom";
 import { Button, Form, FormGroup, Label, Input } from "reactstrap";
 
 import "../../App.css";
@@ -27,71 +26,49 @@ const Contato = ({ match }) => {
             <div className="formulario">
               <Card className="cardcontato" color="secondary">
                 <div>
-                  <Route
-                    exact
-                    path={match.url}
-                    render={() => (
-                      <Form name="contact" method="post">
-                        <input type="hidden" name="form-name" value="contact" />
-                        <FormGroup>
-                          <Label>Nome:</Label>
-                          <Input
-                            type="text"
-                            name="name"
-                            bsSize="sm"
-                            placeholder="Nome ou Empresa"
-                          />
-                        </FormGroup>
-                        <FormGroup>
-                          <Label>Telefone:</Label>
-                          <Input
-                            type="phonenumber"
-                            name="phone"
-                            bsSize="sm"
-                            placeholder=""
-                          />
-                        </FormGroup>
-                        <FormGroup>
-                          <Label>Email:</Label>
-                          <Input
-                            type="email"
-                            name="email"
-                            bsSize="sm"
-                            placeholder=""
-                          />
-                        </FormGroup>
-                        <FormGroup>
-                          <Label>Mensagem:</Label>
-                          <Input
-                            type="textarea"
-                            name="message"
-                            bsSize="sm"
-                            id="exampleText"
-                          />
-                        </FormGroup>
+                  <Form name="contact" method="post">
+                    <input type="hidden" name="form-name" value="contact" />
+                    <FormGroup>
+                      <Label>Nome:</Label>
+                      <Input
+                        type="text"
+                        name="name"
+                        bsSize="sm"
+                        placeholder="Nome ou Empresa"
+                      />
+                    </FormGroup>
+                    <FormGroup>
+                      <Label>Telefone:</Label>
+                      <Input
+                        type="phonenumber"
+                        name="phone"
+                        bsSize="sm"
+                        placeholder=""
+                      />
+                    </FormGroup>
+                    <FormGroup>
+                      <Label>Email:</Label>
+                      <Input
+                        type="email"
+                        name="email"
+                        bsSize="sm"
+                        placeholder=""
+                      />
+                    </FormGroup>
+                    <FormGroup>
+                      <Label>Mensagem:</Label>
+                      <Input
+                        type="textarea"
+                        name="message"
+                        bsSize="sm"
+                        id="exampleText"
+                      />
+                    </FormGroup>
 
-                        <Button color="success" type="submit">
-                          Enviar
-                        </Button>
-                      </Form>
-                    )}
-                  />
-
-                  <Route
-                    path={`${match.url}/:i`}
-                    render={(props) => (
-                      <div className="formulariosuc">
-                        <div className="titulosucesso">
-                          <h3>Formulário Enviado!</h3>
-                          <div className="msgsucesso">
-                            Obrigado, por contatar-nos.
-                            <br />
-                            Em breve entraremos em contato.
-                          </div>
-                        </div>
-                      </div>
-                    )}
-                  />
+                    <Button color="success" type="submit">
+                      Enviar
+                    </Button>
+                  </Form>
                 </div>
               </Card>
             </div>
